b60f9e282c33717aac6474995fc8f9b896439e0d
muskanpunyani <57041968+muskanpunyani@users.noreply.github.com>
2020-05-31 16:45:52 +0530

    Create creating 3d plot.py

---
+++ 3D PLOTTING/creating 3d plot.py
@@ -0,0 +1,9 @@
+from mpl_toolkits import mplot3d    #using matplotlib toolkits
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")      #we want 3d figures
+
+plt.show()          #showing 3d plot
